c57d0a43b6a134312b6c4db141ca5aab14132a59
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-02-26 14:29:58 -0800

    Verify local testing env

--- create-user-web2/function.py
+++ create-user-web2/function.py
@@ -1,15 +1,55 @@
 """
     :module_name: function
-    :module_summary: #TODO
-    :module_author: #TODO
+    :module_summary: Web2 user creation microservice
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
-import sys
+#import uuid
+#import json
+from dataclasses import dataclass
+
 
 def handler(event, context):
-    """A function that returns a simple greeting response"""
+    """
+        A microservice that creates a new idea bank user account using traditional web2 credentials
+    """
+    print(event)
+    print(context)
+    return "Hello from lambda!"
+
+@dataclass
+class Web2AuthKey:
+    """
+        class representing a web2 authkey for a user
+    """
+    email: str
+    pass_hash: str
+    salt: str
+
+@dataclass
+class UserAccount:
+    """
+        class representing an ideabank user account
+    """
+    uuid: str
+    display_name: str
+    auth_keys: dict
+
+def headers() -> dict:
+    """
+        Generate the headers to be included in the response, including any COR related ones
+    """
     return {
-        "sysinfo": sys.version,
-        "event": event,
-        "context": str(context)
-    }
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST'
+            }
+
+def parse_event(event: dict) -> UserAccount:
+    """
+        Parse the data passed to the service as an event object into a more useful state
+        :arg event: the event object triggering this service
+        :arg type: dict
+        :returns: usable data
+        :rtype: UserAccount
+    """
+    print(event)
