65d34cfd199bb4eeec45fb9826e780438d1e60e4
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-02-26 16:06:36 -0800

    Add logging facilities and setup

--- create-user-web2/function.py
+++ create-user-web2/function.py
@@ -4,18 +4,25 @@
     :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
+import logging
 #import uuid
 #import json
+#import base64
 from dataclasses import dataclass
 
+LOGGER = None
+LOG_HANDLER = None
+LOG_FORMAT = None
 
-def handler(event, context):
+
+def handler(event, context): #pylint: disable=unused-argument
     """
         A microservice that creates a new idea bank user account using traditional web2 credentials
     """
-    print(event)
-    print(context)
-    return "Hello from lambda!"
+    set_up_logging()
+    LOGGER.info('Test log')
+    return 'Hello from lambda!'
+
 
 @dataclass
 class Web2AuthKey:
@@ -43,6 +50,19 @@ def headers() -> dict:
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST'
             }
+
+def set_up_logging() -> None:
+    """
+        Set up logging facilities for each run
+    """
+    global LOGGER, LOG_HANDLER, LOG_FORMAT # pylint: disable=global-statement
+    LOGGER = logging.getLogger('create-user-web2-service')
+    LOGGER.setLevel(logging.DEBUG)
+    LOG_HANDLER = logging.StreamHandler()
+    LOG_HANDLER.setLevel(logging.DEBUG)
+    LOG_FORMAT = logging.Formatter('[%(asctime)s:%(levelname)s] - %(message)s')
+    LOG_HANDLER.setFormatter(LOG_FORMAT)
+    LOGGER.addHandler(LOG_HANDLER)
 
 def parse_event(event: dict) -> UserAccount:
     """
