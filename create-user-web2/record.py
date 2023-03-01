be93717324c51f7c2d1937cb59145585cc5991c0
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-01 14:12:02 -0800

    Add class description that is able to communicate with DynamoDB

---
+++ create-user-web2/record.py
@@ -0,0 +1,10 @@
+"""
+    :module_name: record
+    :module_summary: class for interfacing with DynamoDB and creating a user
+    :module_author: Nathan Mendoz (nathancm@uci.edu)
+"""
+
+class IdeaBankUser:
+    """
+        Class that interfaces with DynamDB IdeaBankUser Table
+    """
