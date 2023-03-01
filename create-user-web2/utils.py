068e46c78604836bb1adf1640330f027cc29ff4b
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-01 14:04:42 -0800

    Add constructors to dataclasses

--- create-user-web2/utils.py
+++ create-user-web2/utils.py
@@ -6,6 +6,8 @@
 
 from dataclasses import dataclass
 from uuid import uuid4
+from hashlib import sha256
+from secrets import token_hex
 
 @dataclass
 class UserProfile:
@@ -41,3 +43,21 @@ class Web2Key:
     email: str
     pass_hash: str
     salt: str
+
+    def __init__(self, raw_user, raw_pass):
+        self.email = raw_user
+        self.pass_hash, self.salt = self.__mix_it_up(raw_pass)
+
+    def __mix_it_up(self, thing_to_mix: str) -> (str, str):
+        """
+            Helper method to ensure password is not stored as plain text
+            :arg thing_to_mix: the password to scramble
+            :arg type: str
+            :returns: hash and salt used
+            :rtype: (str, str)
+        """
+        user_salt = token_hex()
+        user_hash = sha256(f'{thing_to_mix}{user_salt}'.encode('utf-8')).hexdigest()
+        return user_hash, user_salt
+
+
