0015e69ee036f838bdadc7422634f1a9d30efccf
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-01 14:11:35 -0800

    Add lambda template

---
+++ create-user-web2/function.py
@@ -0,0 +1,11 @@
+"""
+    :module_name: function
+    :module_summary: main function execution logic
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+from decode import InputDecoder
+from record import IdeaBankUser 
+
+def handler(event, context): #pylint-disable=unused-argument
+    pass
