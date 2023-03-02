ad9710e020776bbb200cec9e6728da83a1634f29
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-01 20:24:18 -0800

    Implement check for operating environment to determine which DB to use

--- create-user-web2/record.py
+++ create-user-web2/record.py
@@ -4,7 +4,20 @@
     :module_author: Nathan Mendoz (nathancm@uci.edu)
 """
 
+import os
+import boto3
+
 class IdeaBankUser:
     """
         Class that interfaces with DynamDB IdeaBankUser Table
     """
+    TABLE_NAME="IdeaBankUser"
+
+    def __init__(self):
+        if os.getenv('ENVIRONMENT') == 'prod':
+            self._resource = boto3.client('dynamodb')
+        else:
+            self._resource = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
+
+    def list_tables(self):
+        print(self._resource.list_tables())
