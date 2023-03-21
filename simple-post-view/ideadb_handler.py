cb53302c2378cf2a0c5c9c69a331b7a19039544e
usampat <usampat@uci.edu>
2023-03-21 10:43:04 -0700

    Added logging to ideadb_handler

--- simple-post-view/ideadb_handler.py
+++ simple-post-view/ideadb_handler.py
@@ -1,5 +1,16 @@
+import logging
 import boto3
 from boto3.dynamodb.conditions import Key
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+LOG_HANDLER = logging.StreamHandler()
+LOG_HANDLER.setLevel(logging.DEBUG)
+
+LOG_FORMAT = logging.Formatter('[%(asctime)s|%(name)s|%(levelname)s] - %(message)s')
+LOG_HANDLER.setFormatter(LOG_FORMAT)
+LOGGER.addHandler(LOG_HANDLER)
 
 # This should be an environment variable
 ENDPOINT_URL = 'http://localhost:8000'
@@ -9,29 +20,47 @@ class IdeaPostTable:
         Encapsulates an Amazon DynamoDB table of post data.
     """
     def __init__(self):
+        LOGGER.info("Started initializing IdeaDB Handler")
+        
         try:
+            LOGGER.debug("Loading boto3 resource for dynamodb...")
             self.dyn_resource = boto3.resource('dynamodb', endpoint_url=ENDPOINT_URL)
             self.table = None
-        except Exception as err: raise DatabaseException(err)
+        except Exception as err: 
+            LOGGER.error("Error initializing boto3 resource for dynamodb with error: %s", str(err))
+            raise DatabaseException(err)
 
     def load(self):
         try:
+            LOGGER.debug("Loading IdeaPost Table...")
             self.table = self.dyn_resource.Table('IdeaPost')
             self.table.load()
-        except Exception as err: raise DatabaseException(err)
+        except Exception as err: 
+            LOGGER.error("Error loading IdeaPost Table with error: %s", str(err))
+            raise DatabaseException(err)
     
     def get_post(self, IdeaPostID, IdeaAuthorID):
         try:
+            LOGGER.debug("Attempting to get post with IdeaPostID: %s and IdeaAuthorID: %s", IdeaPostID, IdeaAuthorID)
             response = self.table.get_item(Key={'IdeaPostID': IdeaPostID, 'IdeaAuthorID': IdeaAuthorID})
-        except Exception as err: raise DatabaseException(err)
+        except Exception as err: 
+            LOGGER.error("Failed to get post with IdeaPostID: %s and IdeaAuthorID: %s with error: %s", IdeaPostID, IdeaAuthorID, str(error))
+
+            raise DatabaseException(err)
         else:
-            if not 'Item' in response: return {}
+            if not 'Item' in response: 
+                LOGGER.debug("Item with IdeaPostID: %s and IdeaAuthorID: %s was NOT FOUND!", IdeaPostID, IdeaAuthorID)
+                return {}
+            LOGGER.debug("Item found: %s", json.dumps(response['Item'], indent=4))
             return response['Item']
 
     def add_post(self, data):
         try:
+            LOGGER.debug("Adding post with data: %s", json.dumps(data, indent=4))
             response = self.table.put_item(Item=data)
-        except Exception as err: raise DatabaseException(err)
+        except Exception as err: 
+            LOGGER.error("Failed to get post with IdeaPostID: %s and IdeaAuthorID: %s with error: %s", IdeaPostID, IdeaAuthorID, str(err))
+            raise DatabaseException(err)
         else: return response
     
 ##################### EXCEPTION #####################
