0d85d737910fddd6d1a05629e88345412646b9d9
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-21 11:29:30 -0700

    Update project structure

---
+++ src/ideabank_webapi/__init__.py
@@ -0,0 +1,6 @@
+"""
+    :module_name: ideabank_webapi
+    :module_summary: An API for the services utilized by the Idea Bank application
+    :module_author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
