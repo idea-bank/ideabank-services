191e2d13bc369eb965ece8ffbc758eee3e417d82
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-21 14:37:19 -0700

    Skip sample tests

--- test/unit/test_ideabank_webapi.py
+++ test/unit/test_ideabank_webapi.py
@@ -4,15 +4,21 @@ import pytest
 
 import ideabank_webapi
 
+
+@pytest.mark.skip
 def test_vacuous():
     """A test that passes vacuously"""
     pass
 
+
+@pytest.mark.skip
 def test_exception():
     """A test that checks if an exception was raised"""
     with pytest.raises(ValueError):
         raise ValueError()
 
+
+@pytest.mark.skip
 @pytest.mark.parametrize("x, y", [(a, b) for a in range(10) for b in range(10)])
 def test_parametrize(x, y):
     """Test cases can be parametrized to test multiple inputs at once"""
