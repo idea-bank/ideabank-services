d4ab4f11efcc803a1f4b7477218dbb7bf60d73c6
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-21 14:35:31 -0700

    Test basic integrity checks for data insertions

--- test/unit/models/test_schema.py
+++ test/unit/models/test_schema.py
@@ -1,22 +1,146 @@
 """Tests for the database schema declarative models"""
 
+import secrets
+import string
+import random
+
 from ideabank_webapi.models import IdeaBankSchema
+from ideabank_webapi.models import Accounts
+from ideabank_webapi.models import Concept
+from ideabank_webapi.models import ConceptLink
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy import insert
+from sqlalchemy.exc import IntegrityError
+
+import pytest
+
+
+@pytest.fixture
+def a_password():
+    return ''.join(random.choices(
+            list(
+                string.ascii_lowercase + string.ascii_uppercase + string.digits
+                ),
+            k=32
+            ))
 
 
 class TestIdeaBankSchema:
-    DB_ENGINE = None
 
-    @classmethod
-    def setup_class(cls):
-        cls.DB_ENGINE = create_engine('sqlite:///:memory:', echo=True)
+    def setup_method(self):
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
+        with Session(self.engine):
+            IdeaBankSchema.metadata.create_all(self.engine)
 
-    @classmethod
-    def teardown_class(cls):
-        cls.DB_ENGINE = None
+    @pytest.mark.parametrize("username", ['user1', 'user2', 'user3'])
+    def test_user_record_creation(self, username, a_password):
+        with Session(self.engine) as session:
+            stmt = insert(Accounts).values(
+                    {
+                        'display_name': username,
+                        'password_hash': a_password,
+                        'salt_value': secrets.token_hex()
+                        }
+                    )
+            session.execute(stmt)
+            session.commit()
 
-    def test_database_schema_is_creatable(self):
-        with Session(self.DB_ENGINE):
-            IdeaBankSchema.metadata.create_all(self.DB_ENGINE)
+    @pytest.mark.parametrize("username", ['user1', 'user2', 'user3'])
+    def test_duplicate_user_cannot_be_created(self, username, a_password):
+        with pytest.raises(IntegrityError):
+            with Session(self.engine) as session:
+                stmt = insert(Accounts).values(
+                        {
+                            'display_name': username,
+                            'password_hash': a_password,
+                            'salt_value': secrets.token_hex()
+                            }
+                        )
+                session.execute(stmt)
+                session.execute(stmt)
+
+    @pytest.mark.parametrize("username, idea", [
+        ('user1', 'idea1'),
+        ('user2', 'idea2'),
+        ('user3', 'idea3')
+        ]
+    )
+    def test_concept_creation(self, username, idea, a_password):
+        with Session(self.engine) as session:
+            stmt = insert(Accounts).values(
+                        {
+                            'display_name': username,
+                            'password_hash': a_password,
+                            'salt_value': secrets.token_hex()
+                            }
+                        )
+            session.execute(stmt)
+            stmt = insert(Concept).values(
+                    {
+                        'author': username,
+                        'title': idea,
+                        'description': '...',
+                        'diagram': '{}'
+                        }
+                    )
+            session.execute(stmt)
+
+    @pytest.mark.parametrize("username, idea", [
+        ('user1', 'idea1'),
+        ('user2', 'idea2'),
+        ('user3', 'idea3')
+        ]
+    )
+    def test_user_cannot_have_duplicate_titles(self, username, idea, a_password):
+        with pytest.raises(IntegrityError):
+            with Session(self.engine) as session:
+                stmt = insert(Accounts).values(
+                            {
+                                'display_name': username,
+                                'password_hash': a_password,
+                                'salt_value': secrets.token_hex()
+                                }
+                            )
+                session.execute(stmt)
+                stmt = insert(Concept).values(
+                        {
+                            'author': username,
+                            'title': idea,
+                            'description': '...',
+                            'diagram': '{}'
+                            }
+                        )
+                session.execute(stmt)
+                session.execute(stmt)
+
+    def test_linking_of_ideas(self, a_password):
+        with Session(self.engine) as session:
+            for username in ['user1', 'user2']:
+                stmt = insert(Accounts).values(
+                            {
+                                'display_name': username,
+                                'password_hash': a_password,
+                                'salt_value': secrets.token_hex()
+                                }
+                            )
+                session.execute(stmt)
+
+                for idea in ['idea1', 'idea2']:
+                    stmt = insert(Concept).values(
+                            {
+                                'author': username,
+                                'title': idea,
+                                'description': '...',
+                                'diagram': '{}'
+                                }
+                            )
+                    session.execute(stmt)
+
+            for start, end in [('user1/idea1', 'user2/idea1'), ('user2/idea2', 'user1/idea1')]:
+                stmt = insert(ConceptLink).values({
+                        'ancestor': start,
+                        'descendant': end
+                        })
+                session.execute(stmt)
