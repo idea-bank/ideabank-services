587776e59577a592840948d21a2c4df9ad4a15fd
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-21 18:34:39 -0700

    Implement the query service class

---
+++ src/ideabank_webapi/services/__init__.py
@@ -0,0 +1,7 @@
+"""
+    :module name: service
+    :module summary: collection of classes that provide access to data services
+    :module author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+from .querydb import QueryService
---
+++ src/ideabank_webapi/services/querydb.py
@@ -0,0 +1,82 @@
+"""
+    :module name: querydb
+    :module summary: Base service class for interacting with application db
+    :module author: Nathan Mendoza (nathancm@uci.edu)
+"""
+
+from contextlib import contextmanager
+from typing import Union, Optional
+
+from ..config import ServiceConfig
+
+from sqlalchemy import create_engine, URL, Result
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.sql.expression import Select, Update, Delete
+
+
+class QueryService:
+    """A class wrapping database connection and transactions
+    Attributes:
+        ENGINE: the db engine used by the service.
+        query_buffer: the list of queued queries to execute
+        results: results of the last executed query
+    """
+    ENGINE = None
+
+    def __init__(self):
+        if not self.ENGINE:
+            self.ENGINE = create_engine(
+                    URL.create(
+                        drivername="postgresql+psycopg",
+                        username=ServiceConfig.DataBase.DBUSER,
+                        password=ServiceConfig.DataBase.DBPASS,
+                        host=ServiceConfig.DataBase.DBHOST,
+                        port=ServiceConfig.DataBase.DBPORT,
+                        database=ServiceConfig.DataBase.DBNAME
+                        )
+                    )
+        self._query_buffer = []
+        self._query_results = None
+
+    @contextmanager
+    def begin_transaction(self):
+        """Provides a SQLAlchemy session to supervise the transaction"""
+        try:
+            session = Session(self.ENGINE)
+            yield session
+            session.commit()
+        except SQLAlchemyError:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def add_query(self, query: Union[Select, Update, Delete]) -> None:
+        """Adds the given query to the query buffer
+        Arguments:
+            query: [Select | Update | Delete] a sqlalchemy stmt
+        Returns:
+            None
+        """
+        self._query_buffer.append(query)
+
+    def exec_next(self, session: Session) -> None:
+        """Use the given session to execute the next query in the buffer
+        Arguments:
+            session: [Session] session to execute the query with
+        Returns:
+            None
+        Raises:
+            IndexError if no next query is queued
+        """
+        stmt = self._query_buffer.pop(0)
+        self._query_results = session.execute(stmt)
+
+    @property
+    def results(self) -> Optional[Result]:
+        """Return the current value of self._query_results
+        Returns:
+            [Optional[Result]]: results of the last query successfully run
+        """
+        return self._query_results
