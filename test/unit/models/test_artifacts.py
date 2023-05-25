d48abae108bb21e68e4847121fb2a4a2d3e628b3
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-24 18:55:55 -0700

    Update credential set test

--- test/unit/models/test_artifacts.py
+++ test/unit/models/test_artifacts.py
@@ -15,33 +15,21 @@ from ideabank_webapi.models import (
 
 def test_valid_credential_set():
     payload = {
-            'display_name': 'dXNlcm5hbWU=',
-            'password': 'cGFzc3dvcmQ='
+            'display_name': 'username',
+            'password': 'password1'
             }
     cred_set = CredentialSet(**payload)
     assert cred_set.display_name == 'username'
-    assert cred_set.password == 'password'
+    assert cred_set.password == 'password1'
 
 
-def test_invalid_credential_set():
+@pytest.mark.parametrize("username, password", [
+    ("user", "s33kret"),
+    ("me", "password1")
+    ])
+def test_invalid_credential_set(username, password):
     payload = {
-            'display_name': 'notadisplayname',
-            'password': 'notapassword'
-            }
-    with pytest.raises(ValidationError):
-        CredentialSet(**payload)
-
-
-@pytest.mark.parametrize("display_name, password", permutations(
-    [
-        'YWJj',
-        'YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWE=',
-    ],
-    2)
-                         )
-def test_misformatted_credential_set(display_name, password):
-    payload = {
-            'display_name': display_name,
+            'display_name': username,
             'password': password
             }
     with pytest.raises(ValidationError):
