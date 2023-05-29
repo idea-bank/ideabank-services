d13f83b3518ca2b456dfb10bb020f2a10fb7eb21
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-28 18:55:39 -0700

    Add handler to retrieve user profile

--- src/ideabank_webapi/handlers/retrievers.py
+++ src/ideabank_webapi/handlers/retrievers.py
@@ -16,9 +16,13 @@ import jwt
 from . import BaseEndpointHandler
 from ..config import ServiceConfig
 from ..services import RegisteredService
-from ..models import CredentialSet, AccountRecord, AuthorizationToken
+from ..models import CredentialSet, AccountRecord, AuthorizationToken, ProfileView
 from ..models import EndpointErrorMessage, EndpointResponse
-from ..exceptions import InvalidCredentialsException, BaseIdeaBankAPIException
+from ..exceptions import (
+        InvalidCredentialsException,
+        BaseIdeaBankAPIException,
+        RequestedDataNotFound
+    )
 
 LOGGER = logging.getLogger(__name__)
 
@@ -26,7 +30,7 @@ LOGGER = logging.getLogger(__name__)
 class AuthenticationHandler(BaseEndpointHandler):
     """Endpoint handler dealing into account authenticaiton"""
 
-    def _do_data_ops(self, request: CredentialSet):
+    def _do_data_ops(self, request: CredentialSet) -> str:
         try:
             LOGGER.info(
                     "Looking up account information: %s",
@@ -95,3 +99,44 @@ class AuthenticationHandler(BaseEndpointHandler):
                 ServiceConfig.AuthKey.JWT_SIGNER,
                 ServiceConfig.AuthKey.JWT_HASHER
                 )
+
+
+class ProfileRetrievalHandler(BaseEndpointHandler):
+    """Endpoint handler dealing with profile retrievals"""
+
+    def _do_data_ops(self, request: str) -> ProfileView:
+        try:
+            LOGGER.info("Looking up profile information: %s", request)
+            with self.get_service(RegisteredService.ACCOUNTS_DS) as service:
+                service.add_query(service.fetch_account_profile(
+                    display_name=request
+                    ))
+                service.exec_next()
+                result = service.results.one()
+                return ProfileView(
+                    preferred_name=result.preferred_name,
+                    biography=result.biography,
+                    avatar_url=service.share_item(f'avatars/{request}')
+                        )
+        except NoResultFound as err:
+            LOGGER.error("No account record found: %s", request)
+            raise RequestedDataNotFound(
+                    f'Profile for {request} is not available'
+                    ) from err
+
+    def _build_success_response(self, requested_data: ProfileView):
+        self._result = EndpointResponse(
+                code=status.HTTP_200_OK,
+                body=requested_data
+                )
+
+    def _build_error_response(self, exc: BaseIdeaBankAPIException):
+        if isinstance(exc, RequestedDataNotFound):
+            self._result = EndpointResponse(
+                    code=status.HTTP_404_NOT_FOUND,
+                    body=EndpointErrorMessage(
+                        err_msg=str(exc)
+                        )
+                    )
+        else:
+            super()._build_error_response(exc)
