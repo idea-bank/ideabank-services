68e1c7da8f5b2bcdfc75a1379b5ca9734f3c8041
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-29 15:35:31 -0700

    Tests for concept creation handler

--- test/unit/handlers/test_creators.py
+++ test/unit/handlers/test_creators.py
@@ -3,18 +3,55 @@
 import pytest
 from unittest.mock import patch
 from ideabank_webapi.handlers import EndpointHandlerStatus
-from ideabank_webapi.handlers.creators import AccountCreationHandler
-from ideabank_webapi.services import RegisteredService, AccountsDataService, QueryService
+from ideabank_webapi.handlers.creators import AccountCreationHandler, ConceptCreationHandler
+from ideabank_webapi.handlers.preprocessors import AuthorizationRequired
+from ideabank_webapi.services import (
+        RegisteredService,
+        AccountsDataService,
+        QueryService,
+        ConceptsDataService
+        )
 from ideabank_webapi.models import (
         CredentialSet,
         EndpointInformationalMessage,
         EndpointErrorMessage,
+        AuthorizationToken,
+        ConceptSimpleView,
+        ConceptDataPayload,
+        CreateConcept,
         IdeaBankSchema
 )
-from ideabank_webapi.exceptions import BaseIdeaBankAPIException
+from ideabank_webapi.exceptions import BaseIdeaBankAPIException, NotAuthorizedError
 
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from fastapi import status
+
+
+@pytest.fixture
+def test_auth_token():
+    return AuthorizationToken(
+            token='testtoken',
+            presenter='testuser'
+            )
+
+
+@pytest.fixture
+def test_concept_payload():
+    return ConceptDataPayload(
+            author='testuser',
+            title='sample-idea',
+            description='An example of a really cool idea',
+            diagram={}
+            )
+
+
+@pytest.fixture
+def test_concept_simple_view():
+    return ConceptSimpleView(
+            identifier='testuser/sample-idea',
+            thumbnail_url='http://example.com/thumbnails/testuser/sample-idea'
+            )
 
 
 @patch.object(QueryService, 'ENGINE', create_engine('sqlite:///:memory:', echo=True))
@@ -78,3 +115,118 @@ class TestAccountCreationHandler:
         assert self.handler.result.body == EndpointErrorMessage(
                 err_msg='Really obscure error'
                 )
+
+
+@patch.object(QueryService, 'ENGINE', create_engine('sqlite:///:memory:', echo=True))
+@patch.object(QueryService, 'exec_next')
+@patch.object(QueryService, 'results')
+class TestConceptCreationHandler:
+
+    def setup_method(self):
+        self.handler = ConceptCreationHandler()
+        self.handler.use_service(RegisteredService.CONCEPTS_DS, ConceptsDataService())
+
+    @patch.object(AuthorizationRequired, '_check_if_authorized')
+    @patch.object(ConceptsDataService, 'share_item')
+    def test_successful_concept_creation(
+        self,
+        mock_s3_url,
+        mock_auth_check,
+        mock_query_result,
+        mock_query,
+        test_auth_token,
+        test_concept_payload,
+        test_concept_simple_view
+    ):
+        mock_query_result.one.return_value = test_concept_simple_view
+        mock_s3_url.return_value = test_concept_simple_view.thumbnail_url
+        self.handler.receive(CreateConcept(
+            auth_token=test_auth_token,
+            **test_concept_payload.dict()
+        ))
+        assert self.handler.status == EndpointHandlerStatus.COMPLETE
+        assert self.handler.result.code == status.HTTP_201_CREATED
+        assert self.handler.result.body == test_concept_simple_view
+        mock_s3_url.assert_called_once_with(f'thumbnails/{test_concept_payload.author}/{test_concept_payload.title}')
+        mock_query_result.one.assert_called_once()
+        mock_query.assert_called_once()
+        mock_auth_check.assert_called_once_with(test_auth_token)
+
+    @patch.object(AuthorizationRequired, '_check_if_authorized')
+    @patch.object(ConceptsDataService, 'share_item')
+    def test_duplicate_concept_creation(
+        self,
+        mock_s3_url,
+        mock_auth_check,
+        mock_query_result,
+        mock_query,
+        test_auth_token,
+        test_concept_payload,
+        test_concept_simple_view
+    ):
+        mock_query.side_effect = IntegrityError("doing", "a", "test")
+        self.handler.receive(CreateConcept(
+            auth_token=test_auth_token,
+            **test_concept_payload.dict()
+        ))
+        assert self.handler.status == EndpointHandlerStatus.ERROR
+        assert self.handler.result.code == status.HTTP_403_FORBIDDEN
+        assert self.handler.result.body == EndpointErrorMessage(
+                err_msg=f'{test_concept_payload.author}/{test_concept_payload.title} is not available'
+                )
+
+    @pytest.mark.parametrize("err_type, err_msg", [
+        (NotAuthorizedError, 'Invalid token presented'),
+        (NotAuthorizedError, 'Unable to verify token ownership')
+        ])
+    @patch.object(AuthorizationRequired, '_check_if_authorized')
+    @patch.object(ConceptsDataService, 'share_item')
+    def test_unauthorized_concept_creation(
+        self,
+        mock_s3_url,
+        mock_auth_check,
+        mock_query_result,
+        mock_query,
+        test_auth_token,
+        test_concept_payload,
+        test_concept_simple_view,
+        err_type,
+        err_msg
+    ):
+        mock_auth_check.side_effect = err_type(err_msg)
+        self.handler.receive(CreateConcept(
+            auth_token=test_auth_token,
+            **test_concept_payload.dict()
+        ))
+        assert self.handler.status == EndpointHandlerStatus.ERROR
+        assert self.handler.result.code == status.HTTP_401_UNAUTHORIZED
+        assert self.handler.result.body == EndpointErrorMessage(
+                err_msg=err_msg
+                )
+
+    @patch.object(
+            ConceptCreationHandler,
+            '_do_data_ops',
+            side_effect=BaseIdeaBankAPIException("Really obscure error")
+        )
+    @patch.object(AuthorizationRequired, '_check_if_authorized')
+    def test_a_really_messed_up_scenario(
+            self,
+            mock_auth_check,
+            mock_data_ops,
+            mock_query_result,
+            mock_query,
+            test_concept_payload,
+            test_auth_token
+            ):
+        self.handler.receive(CreateConcept(
+            auth_token=test_auth_token,
+            **test_concept_payload.dict()
+        ))
+        assert self.handler.status == EndpointHandlerStatus.ERROR
+        assert self.handler.result.code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert self.handler.result.body == EndpointErrorMessage(
+                err_msg='Really obscure error'
+                )
+
+
