578ae26cdb3858a601d9d636bf7f985a31d42e78
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-05-30 19:27:16 -0700

    Tests for concept query search params

--- test/unit/models/test_artifacts.py
+++ test/unit/models/test_artifacts.py
@@ -1,12 +1,15 @@
 """Tests for api artifact models"""
 
-import pytest
+import datetime
 
+import pytest
+from freezegun import freeze_time
 from pydantic import ValidationError
 
 from ideabank_webapi.models import (
         CredentialSet,
-        AccountRecord
+        AccountRecord,
+        ConceptSearchQuery
         )
 
 
@@ -53,10 +56,38 @@ def test_valid_account_record():
     ('username5', 64 * 'z', 64 * 'b'),  # hash not hex
     ('username6', 64 * 'a', 64 * 'y')   # salt not hex
     ])
+@pytest.mark.xfail(raises=ValidationError)
 def test_invalid_account_record(username, hash, salt):
-    with pytest.raises(ValidationError):
-        AccountRecord(
-                display_name=username,
-                password_hash=hash,
-                salt_value=salt
-                )
+    AccountRecord(
+            display_name=username,
+            password_hash=hash,
+            salt_value=salt
+            )
+
+
+@freeze_time("2023-01-16 18:30:11")
+def test_default_date_range_in_search_params():
+    search_params = ConceptSearchQuery(
+            title='atitle',
+            author='anauthor'
+            )
+    assert search_params.title == 'atitle'
+    assert search_params.author == 'anauthor'
+    assert search_params.not_before == datetime.datetime.fromtimestamp(0, datetime.timezone.utc)
+    assert search_params.not_after == datetime.datetime.now(datetime.timezone.utc)
+    assert search_params.fuzzy is False
+
+
+@freeze_time("2023-01-16 18:30:11")
+def test_non_default_date_range_in_search_params():
+    search_params = ConceptSearchQuery(
+            title='atitle',
+            author='anauthor',
+            not_before=datetime.datetime.utcnow() - datetime.timedelta(days=4),
+            not_after=datetime.datetime.utcnow() - datetime.timedelta(days=2)
+            )
+    assert search_params.title == 'atitle'
+    assert search_params.author == 'anauthor'
+    assert search_params.not_before == datetime.datetime.utcnow() - datetime.timedelta(days=4)
+    assert search_params.not_after == datetime.datetime.utcnow() - datetime.timedelta(days=2)
+    assert search_params.fuzzy is False
