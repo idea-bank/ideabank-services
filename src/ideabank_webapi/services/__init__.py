c042badf08907ae35ac753315096e5d51dfae0cc
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-06-03 14:36:46 -0700

    Register the engagement service

--- src/ideabank_webapi/services/__init__.py
+++ src/ideabank_webapi/services/__init__.py
@@ -10,6 +10,7 @@ from .querydb import QueryService
 from .s3crud import S3Crud
 from .accounts import AccountsDataService
 from .concepts import ConceptsDataService
+from .engage import EngagementDataService
 
 
 class RegisteredService(Enum):
@@ -18,3 +19,4 @@ class RegisteredService(Enum):
     RAW_S3 = S3Crud
     ACCOUNTS_DS = AccountsDataService
     CONCEPTS_DS = ConceptsDataService
+    ENGAGE_DS = EngagementDataService
